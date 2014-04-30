7d93c05fe8813acdbba7f83665465ca3a3b7d56e
liz tom & Maureen Dugan <student@epicodus.com>
2014-04-30 11:59:50 -0700

    add show team template

--- js/router.js
+++ js/router.js
@@ -1,12 +1,19 @@
 Stats.Router.map(function() {
   this.resource('teams', { path: '/' }, function(){
-    this.resource('new_team', { path: '/teams/new'})
+    this.resource('new_team', { path: '/teams/new'});
+    this.resource('team', { path: '/teams/:id'});
   });
 });
 
 Stats.TeamsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('team');
+  }
+});
+
+Stats.TeamRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('team', params.id);
   }
 });
 
